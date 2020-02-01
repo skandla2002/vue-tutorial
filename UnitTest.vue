4da4cec977841debe39e805d2976341ded723ffb
JONGMEONG PAK <skandla2002@gmail.com>
2020-02-02 00:23:40 +0900

    git unit test 단계 커밋

---
+++ UnitTest.vue
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import MyComponent from 'MyComponent.vue';
+
+describe('MyComponent', () => {
+    it('has a created hook', () => {
+        expect(typeof MyComponent.created).toBe('function')
+    })
+
+    it('sets the correct default data', () => {
+        expect(typeof MyComponent.data).toBe('function')
+        const defaultData = MyComponent.data()
+        expect(defaultData.message).toBe('hello')
+    })
+
+    it('correctly sets the message when created', () => {
+        const vm = new Vue(MyComponent).$mount()
+        expect(vm.message).toBe('Bye!')
+    })
+
+    if('renders the correct message', () => {
+        const Constructor = Vue.extend(MyComponent)
+        const vm = new Constructor().$mount()
+        expect(vm.$el.textContent).toBe('bye!')
+    })
+})
